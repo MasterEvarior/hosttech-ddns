969a8c60be2b89b1d9be550d0d6367470c483b74
MasterEvarior <contact@giannin.dev>
2023-07-17 20:07:39 +0200

    Improved logging

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-
 	for true {
-		time.Sleep(1200 * time.Second)
 		ip, err := GetPublicIp()
 		if err != nil {
 			log.Printf("Could not fetch public IP because of an error: %s", err)
 			continue
 		}
-
 		log.Printf("Got public IP %s", ip)
 
 		UpdateHosttechRecords(ip)
+
+		log.Printf("Waiting for next update...")
+		time.Sleep(1200 * time.Second)
 	}
 }
